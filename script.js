5712d9e9d74170af9105b10e6955078d7ec7ba90
aloelsafzi <samsulfauzialoel@gmail.com>
2023-10-27 14:19:04 +0700

    fix get data address from drag marker and search with revese geolocation too

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 $(document).ready(() => {
+  // init 
   const locDefault = {lat: -6.2882807, lon: 106.7582081}
+  getAddressFromLocation ([locDefault.lat,locDefault.lon])
+
   const map = L.map('map').setView([locDefault.lat,locDefault.lon], 15);
   let marker = L.marker([locDefault.lat,locDefault.lon],{draggable: true, autoPanOnFocus: true}).addTo(map);
 
@@ -17,6 +20,7 @@ $(document).ready(() => {
         return {
           q: params.term, // search term
           polygon_geojson: 1,
+          addressdetails: 1,
           format: 'jsonv2'
         };
       },
@@ -75,28 +79,48 @@ $(document).ready(() => {
     map.flyToBounds([location])
   });
   
-  $mySelect.on('select2:clear', function (e) {
-    $('.geo').text('Tidak ada data')
+  $mySelect.on('select2:clear', function (e) {    
+    const location = [locDefault.lat,locDefault.lon]
+    marker.setLatLng(location,{draggable:'true'}).bindPopup(location).update()
+    map.flyToBounds([location])
   });
   
 
-  function onMapClick(e) {      
+  async function onMapClick(e) {      
     marker.setLatLng([e.latlng.lat, e.latlng.lng],{draggable:'true'})
-    let text = `latitude: ${e.latlng.lat}, longitude: ${e.latlng.lng}`
-    $('.geo').text(text)
     map.addLayer(marker);
+    await getAddressFromLocation ([e.latlng.lat,e.latlng.lng])
   }
 
-  marker.on('dragend', function(event){
+  marker.on('dragend', async function(event){
     marker = event.target;
     const position = marker.getLatLng();
     const {lat,lng} = position
     
     marker.setLatLng([lat,lng],{draggable:'true'}).bindPopup([lat,lng]).update();
-    text = `latitude: ${lat}, longitude: ${lng}`
-    $('.geo').text(text)
+    await getAddressFromLocation ([lat,lng])
   });
+
   map.addLayer(marker);
 
   map.on('click', onMapClick);
+
+  async function getAddressFromLocation (location) {
+    const [lat, lon] = location
+    $('.geo').text('Loading...')
+    const response = await $.ajax({
+      method: "GET",
+      url: `https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${lat}&lon=${lon}`,
+      dataType: "json"
+    });
+
+    const text = response.display_name
+    const value = `${response.lat},${response.lon}`
+    const op = new Option(text, value, true, true)
+    $('.js-example-data-ajax').append(op)
+
+    let textView = `latitude: ${response.lat}, longitude: ${response.lon}`
+    $('.geo').text(textView)
+  }  
+
 })
